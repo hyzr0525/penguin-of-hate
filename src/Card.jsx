9711cfa200ffb4bd200b001214040f17585edcd3
Hathor Hamilton <koshknot@gmail.com>
2021-10-05 10:25:07 -0400

    Added random functionality

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,15 @@
 
 
-function Card({cards}){
+function Card({card}){
 
 
 return(
     <div>
-    <img src={cards.name_short} />
-    <p>{cards.name}</p>
-    <p>{cards.type}</p>
-    <p>{cards.desc}</p>
+    <img src={card.name_short} />
+    <p><em>{card.name}</em></p>
+    <p>Arcana: {card.type}</p>
+    <p>Meaning: {card.meaning_up}</p>
+    <p>Card Description: {card.desc}</p>
     <hr/>
     </div>
 )
--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -1,14 +1,37 @@
 import Card from "./Card"
 import Sidebar from "./Sidebar"
 import "./App.css"
+import React, {useState, useEffect} from 'react';
+
+function CardContainer(){
+    // states
+    const URL = "http://localhost:4000/cards"
+    const [cardData, setCardData] = useState([])
+    const [readCard , setReadCard] = useState(false)
+    //variables
+    const randomCardData = cardData[Math.floor(Math.random() * cardData.length)]
+    
 
 
-function CardContainer({cardData}){
+    // functions
+    function showCard(){
+        setReadCard(!readCard)
+    }
+
+
+    //init fetch
+    useEffect(()=>{
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => setCardData(data))
+    },[])
+
 
     return (
         <>
         <div className="MainContent">
-        {cardData.map(cards => <Card cards= {cards}/>)}
+            <button onClick={showCard}>Get a Reading!</button>
+            {readCard? <Card card ={randomCardData}/> : null}
         </div>
         <div className="SideBar">
             {cardData.map(cards => <Sidebar cards={cards}/>)}
