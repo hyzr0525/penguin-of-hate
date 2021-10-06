2f109312bdc7ab032afbe0688c8ab39a2e187516
Hathor Hamilton <koshknot@gmail.com>
2021-10-06 11:32:03 -0400

    minor change

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -9,6 +9,7 @@ function Sidebar({cards , history, setReadCard}){
             
             <h3>History</h3>
             <History history={history} setReadCard={setReadCard}/>
+            <h3>Card Search</h3>
             <Search />
             <h3>Card List</h3>
             {cards.map((card)=> <ul key={card.name} className="SidebarList">{card.name}</ul>)}
