09517dcc58c36b046f3b656e436731f4a3b57a79
Hathor Hamilton <koshknot@gmail.com>
2021-10-07 11:32:57 -0400

    added user forms

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,10 +2,11 @@ import History from "./History"
 import Search from "./SearchBar"
 import {useState} from "react"
 import {NavLink} from "react-router-dom"
+import UserForms from "./UserForms"
 
 
 function Sidebar({cards , history, setReadCard}){
-
+    const [show ,setShow] = useState(false) 
     const [searchKeyWord, setSearchKeyWord] = useState("")
 
     const filterCards = cards.filter((filteredCard) =>{
@@ -14,7 +15,9 @@ function Sidebar({cards , history, setReadCard}){
    
     return(
     <>
-            
+        <h3>User Login</h3>
+        <button onClick={()=>setShow(!show)}>{!show? 'New User?' : "Returning User" }</button>
+        <UserForms show={show} setShow={setShow}/>    
         <h3>History</h3>
         <History history={history} setReadCard={setReadCard}/>
         <h3>Card Search</h3>
---
+++ src/UserForms.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+function UserForms({show, setShow}){
+    const userUrl = "http://localhost:4000/users"
+    const [currentUser, setCurrentUser] = useState(null)
+    const [input , setInput] = useState({
+        "username" : "",
+        "password" : "",
+        })
+
+    
+    function formFill(e){
+        setInput({...input,
+        [e.target.name]:e.target.value})
+        
+    }    
+    
+    function newUser(e){
+        e.preventDefault()
+        fetch(userUrl,{
+            method : 'POST',
+            headers :{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(input)
+            })
+        .then(res=>res.json)
+        .then(data =>console.log(data))
+        e.target.reset()
+        setShow(!show)
+        
+
+    }
+    function logIn(e){
+        e.preventDefault()
+    fetch(userUrl)
+    .then(res=>res.json())
+    .then(users => {
+        const user = users.filter((user)=> user.username == input.username)
+        setCurrentUser(user)
+        // user.password === input.password? setCurrentUser(user) : alert("Password does not Match")
+    } )
+    
+    }
+    console.log(currentUser)
+    if (currentUser !== null){
+        return(
+            <>
+        <h4>Welcome, {input.username}</h4>
+        
+        </>
+        )}
+    if (show){
+        return (
+            <form onSubmit={(e)=>newUser(e)}>
+                <input type='text' name='username' placeholder='New Username' onChange={(e)=> formFill(e)}/>
+                <input type='text' name='password' placeholder='NewPassword'onChange={(e)=> formFill(e)}/>
+                <input type='submit' />
+            </form>
+        )
+    }
+    return (
+    
+    <form onSubmit={(e)=>logIn(e)}>
+        <input type='text' name='username' placeholder='Input Username'onChange={(e)=> formFill(e)}/>
+        <input type='text' name='password' placeholder='Input Password'onChange={(e)=> formFill(e)}/>
+        <input type='submit'/>
+    </form>
+
+    )
+
+}
+
+export default UserForms
